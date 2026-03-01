5c630975125c00c318b0cec316253080caded0b7
Jinyoung <glitt5384@naver.com>
2026-03-01 22:29:04 +0900

    feat: ProjectGithubIssue 배열 추가

--- src/main/java/com/capstone/domain/project/entity/ProjectOrganization.java
+++ src/main/java/com/capstone/domain/project/entity/ProjectOrganization.java
@@ -1,21 +1,28 @@
 package com.capstone.domain.project.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @EqualsAndHashCode
 public class ProjectOrganization {
     private String orgName;
-    private List<String> projectRepos;
+    private List<String> orgRepos;
+    private List<ProjectGithubIssue> githubIssues = new ArrayList<>();
 
-    public static ProjectOrganization create(String orgName, List<String> projectRepos) {
-        return new ProjectOrganization(orgName, projectRepos);
+    public static ProjectOrganization create(String orgName, List<String> orgRepos) {
+        return ProjectOrganization.builder()
+                .orgName(orgName)
+                .orgRepos(orgRepos)
+                .build();
+    }
+
+    public void addGithubIssue(ProjectGithubIssue issue) {
+        this.getGithubIssues().add(issue);
     }
 }
