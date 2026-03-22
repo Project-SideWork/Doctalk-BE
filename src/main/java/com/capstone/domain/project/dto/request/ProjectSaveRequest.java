1d0a5a4ce58cc5759b85e2290e67a25d9b42e058
Jinyoung <glitt5384@naver.com>
2026-03-23 01:46:19 +0900

    fix: notnull -> notblank

--- src/main/java/com/capstone/domain/project/dto/request/ProjectSaveRequest.java
+++ src/main/java/com/capstone/domain/project/dto/request/ProjectSaveRequest.java
@@ -1,21 +1,24 @@
 package com.capstone.domain.project.dto.request;
 
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 public record ProjectSaveRequest(
         @Nullable
         String projectId,
-        @NotNull
+        @NotBlank
         @Size(min = 1)
         String projectName,
-        @NotNull
+        @NotBlank
         @Size(min = 1)
         String description,
         @Nullable
         List<String> invitedEmails,
+        @Nullable
+        @Validated
         List<ProjectGithubInfo> githubInfos
 ){}
