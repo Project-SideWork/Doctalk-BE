76069e12666a7b03ac94f5b4800ca1413faae241
Jinyoung <glitt5384@naver.com>
2026-03-24 13:11:44 +0900

    fix: 쿠키 생성 및 삭제 로직 제거

--- src/main/java/com/capstone/global/jwt/CookieUtil.java
+++ src/main/java/com/capstone/global/jwt/CookieUtil.java
@@ -17,35 +17,6 @@ import java.util.Optional;
 
 @Component
 public class CookieUtil {
-    public static final int COOKIE_EXPIRE_TIME = 30 * 60; // 30분
-    private static final ObjectMapper objectMapper = new ObjectMapper()
-            .registerModule(new CoreJackson2Module())
-            .registerModule(new OAuth2ClientJackson2Module());
-
-    public Cookie createCookie(String key, String value) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setPath("/");
-        cookie.setMaxAge(COOKIE_EXPIRE_TIME);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);  // HTTPS 요청에만 secure 설정
-        cookie.setAttribute("SameSite", "Strict");
-        return cookie;
-    }
-
-    public static void expireCookie(
-            HttpServletResponse res, String name, String path,
-            boolean httpOnly, boolean secure, String sameSite
-    ) {
-        Cookie cookie = new Cookie(name, "");
-        cookie.setPath(path);
-        cookie.setHttpOnly(httpOnly);
-        cookie.setSecure(secure);
-        cookie.setMaxAge(0);
-
-        cookie.setAttribute("SameSite", sameSite);
-        res.addCookie(cookie);
-    }
-
     public static String getAccessTokenFromRequest(HttpServletRequest request) {
         if (request.getCookies() == null) {
             return null;
@@ -55,57 +26,5 @@ public class CookieUtil {
                 .map(Cookie::getValue)
                 .findFirst()
                 .orElse(null);  // Optional을 String으로
-    }
-
-    public static String getRefreshTokenFromRequest(HttpServletRequest request) {
-        if (request.getCookies() == null) {
-            return null;
-        }
-        return Arrays.stream(request.getCookies())
-                .filter(cookie -> "refresh".equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElse(null);  // Optional을 String으로
-    }
-
-    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
-                    return Optional.of(cookie);
-                }
-            }
-        }
-        return Optional.empty();
-    }
-
-    // TODO: setDomain
-    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setMaxAge(maxAge);
-        cookie.setPath("/");
-        // cookie.setDomain("");
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setAttribute("SameSite", "Lax");
-        response.addCookie(cookie);
-    }
-
-
-    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(name)) {
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    cookie.setMaxAge(0);
-                    cookie.setSecure(true);
-                    cookie.setAttribute("SameSite", "None");
-                    response.addCookie(cookie);
-                }
-            }
-        }
     }
 }
