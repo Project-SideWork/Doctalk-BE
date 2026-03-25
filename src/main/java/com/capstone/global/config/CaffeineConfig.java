2930fc24397fd703b13e2b80aabaa3e0f3ed3aab
Jinyoung <glitt5384@naver.com>
2026-03-26 03:43:56 +0900

    feat: 카페인 캐시 빈 등록

--- src/main/java/com/capstone/global/config/CaffeineConfig.java
+++ src/main/java/com/capstone/global/config/CaffeineConfig.java
@@ -1,24 +1,35 @@
-//package com.capstone.global.config;
-//
-//
-//import com.github.benmanes.caffeine.cache.Caffeine;
-//import org.springframework.cache.CacheManager;
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.cache.caffeine.CaffeineCacheManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@Configuration
-//@EnableCaching
-//public class CaffeineConfig {
-//    @Bean
-//    public CacheManager cacheManager() {
-//        CaffeineCacheManager cacheManager = new CaffeineCacheManager("githubAccessToken");
-//        cacheManager.setCaffeine(Caffeine.newBuilder()
-//                .expireAfterWrite(1, TimeUnit.HOURS)
-//                .maximumSize(1000));
-//        return cacheManager;
-//    }
-//}
+package com.capstone.global.config;
+
+
+import com.capstone.domain.github.dto.response.GithubInfoResponse;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CaffeineConfig {
+
+    @Bean
+    public Cache<Long, GithubInfoResponse> githubTokenCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.HOURS)
+                .maximumSize(1000)
+                .build();
+    }
+
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("githubAccessToken");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.HOURS)
+                .maximumSize(1000));
+        return cacheManager;
+    }
+}
