c6de680a7d4cf5f6ee98fdaf197f0264ed9c1ef8
Jinyoung <glitt5384@naver.com>
2026-03-26 23:39:45 +0900

    fix: UserDetailsService 제거

--- src/main/java/com/capstone/domain/user/entity/User.java
+++ src/main/java/com/capstone/domain/user/entity/User.java
@@ -1,13 +1,11 @@
 package com.capstone.domain.user.entity;
 
-import com.capstone.domain.auth.register.dto.RegisterRequest;
 import com.capstone.domain.mypage.dto.UserDto;
 import com.capstone.global.entity.BaseDocument;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
